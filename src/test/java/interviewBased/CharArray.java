1d56a921679b82783e8b6de0fed2bf1ebcee3b8c
Ashish Anarase <anaraseashish@gmail.com>
2024-08-23 10:31:43 +0530

    new

---
+++ src/test/java/interviewBased/CharArray.java
@@ -0,0 +1,12 @@
+package interviewBased;
+
+public class CharArray {
+	
+	public static void main(String[] args) {
+		
+		char [] ch = {'a','s','h','i','s','h'};
+		
+		System.out.println(ch);
+	}
+
+}
