fca3af21b60e3c1a27a8d9f682c7426f709eb25d
Ashish Anarase <anaraseashish@gmail.com>
2024-10-09 10:06:35 +0530

    Swap string Updated

--- src/test/java/interviewBased/SwapString.java
+++ src/test/java/interviewBased/SwapString.java
@@ -4,16 +4,38 @@ public class SwapString {
 
 	public static void main(String[] args) {
 
-		String new1 = "Ashish Anarase";
-		String new2 = "Ashish Jayram Anarase";
+		System.out.println("Operation using third variable");
+		
+		String new1 = "Apple";
+		String new2 = "Banana";
+		
+		System.out.println("Before swaping String 1 : " +new1);
+		System.out.println("Before swaping String 2 : "+new2);
 
 		String temp = new2;
 		new2 = new1;
 		new1 = temp;
 
-		System.out.println(new1);
-		System.out.println(new2);
+		System.out.println("After swaping String 1 : "+ new1);
+		System.out.println("After swaping String 2 : "+new2);
 
+		System.out.println();
+		System.out.println("Operation without using third variable");
+		
+		String new3 = "Safarchand";
+		String new4 = "Keli";		
+		
+		System.out.println("Before swaping String 3 : " +new3);
+		System.out.println("Before swaping String 4 : "+new4);
+		
+		new3 = new3 + new4;
+		
+		new4 = new3.substring(0, new3.length()-new4.length());
+		
+		new3 = new3.substring(new4.length());
+		
+		System.out.println("After swaping String 3 : "+ new3);
+		System.out.println("After swaping String 4 : "+new4);
 	}
 }
 
