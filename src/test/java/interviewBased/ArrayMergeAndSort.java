3fdbefe8d71f0b1a46d1baacd9f6dc407399d8a2
Ashish <anaraseashish@gmail.com>
2024-12-18 13:23:39 +0530

    Array Sort Modified

--- src/test/java/interviewBased/ArrayMergeAndSort.java
+++ src/test/java/interviewBased/ArrayMergeAndSort.java
@@ -1,28 +1,39 @@
 package interviewBased;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class ArrayMergeAndSort {
-	
+
 	public static void main(String[] args) {
-		
+
 		int[] arr1 = {1, 4, 6, 8};
-        int[] arr2 = {3, 5, 6, 7};
-        
-        ArrayList ar = new ArrayList();
-        
-        for (int i = 0; i<= arr1.length-1; i++) {
-       
-        ar.add(arr1[i]);
-        ar.add(arr2[i]);
-        
-        }
-        System.out.println("Merged Array : "+ar);
-        Collections.sort(ar);
-        System.out.println("Sorted Array : "+ar);
-        
+		int[] arr2 = {3, 5, 6, 7};
 		
+		System.out.println("Given Array 1 : "+Arrays.toString(arr1));
+		
+		System.out.println("Given Array 2 : "+Arrays.toString(arr2));
+
+		ArrayList ar = new ArrayList();
+
+		for (int i = 0; i<= arr1.length-1; i++) {
+
+			ar.add(arr1[i]);
+			ar.add(arr2[i]);
+
+		}
+		System.out.println("Merged Array : "+ar);
+		
+		Collections.sort(ar);
+		
+		System.out.println("Sorted Array in ascending order : "+ar);
+
+		Collections.sort(ar, Collections.reverseOrder());
+
+		System.out.println("Sorted Array in descending order : "+ar);
+
+
 	}
 
 }
