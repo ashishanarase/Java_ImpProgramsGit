75a3ed31d8ee0805e85c9f7d8d6f613f884c7b32
Ashish <anaraseashish@gmail.com>
2025-01-23 01:36:56 +0530

    Neew

--- src/test/java/interviewBased/UnixCommandListStored.java
+++ src/test/java/interviewBased/UnixCommandListStored.java
@@ -10,7 +10,7 @@ chmod	-- Changing Permissions
 cp  -- Copy source file into destination
 file -- Determine file type
 find -- Find files
-grep -- Search files for regular expressions.
+grep -- Search files for regular expressions and search specific string.
 head -- Display first few lines of a file
 ln  -- Create softlink on oldname
 ls  -- Display information about file type.
@@ -21,8 +21,13 @@ pwd -- Print current working directory.
 rm  -- Remove (Delete) filename
 rmdir -- Delete an existing directory provided it is empty.
 tail -- Prints last few lines in a file.
-touch -- Update access and modification time of a file.
+touch -- Update access and modification time of a file and To create file.
+wc -- Word Count
+man -- User manual
+vi -- to modify the data of an existing file
 $  -- Represents a new Channel line which is open to perform operation
+less -- to display the content of the file page by page
+more -- to display the content of the file page by page along with percentage
 */
 
 }
