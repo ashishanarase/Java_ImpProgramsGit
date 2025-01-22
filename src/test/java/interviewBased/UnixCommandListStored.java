8527a6090d3ec845e4d2c882d053babd9c32da58
Ashish <anaraseashish@gmail.com>
2025-01-23 01:29:30 +0530

    UN

--- src/test/java/interviewBased/UnixCommandListStored.java
+++ src/test/java/interviewBased/UnixCommandListStored.java
@@ -22,6 +22,7 @@ rm  -- Remove (Delete) filename
 rmdir -- Delete an existing directory provided it is empty.
 tail -- Prints last few lines in a file.
 touch -- Update access and modification time of a file.
+$  -- Represents a new Channel line which is open to perform operation
 */
 
 }
