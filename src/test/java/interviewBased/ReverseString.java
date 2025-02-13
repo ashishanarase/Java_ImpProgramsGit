ea9d3604299cfdd896e66a8694410e5755f0d5ae
Ashish <anaraseashish@gmail.com>
2025-02-13 23:56:52 +0530

    new

--- src/test/java/interviewBased/ReverseString.java
+++ src/test/java/interviewBased/ReverseString.java
@@ -3,14 +3,28 @@ package interviewBased;
 public class ReverseString {
 	
 	public static void main (String [] args) {
-		
+		//Conventionally
 		String name = "Ashish";
 		
 		int length = name.length();
 		
 		for (int i = length-1;i >=0 ; i--) {
 			System.out.print(name.charAt(i));
+		}	
+		System.out.println();
+		//Using Pre built methods
+		String sample = "Anarase";
+		
+		StringBuilder build = new StringBuilder(sample);
+		
+		//StringBuffer build = new StringBuffer(sample);  // Any option we can use
+
+		
+		String reverse = build.reverse().toString();
+		
+		System.out.println(reverse);
+		
 		}
 	}
 
-}
+
